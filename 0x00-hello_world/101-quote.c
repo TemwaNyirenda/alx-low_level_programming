8c9278677baafe7d0782e55039a1ac3aa29501cb
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-06-02 00:02:18 -0700

    Task #8, added file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print quote using fwrite to standard error
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",    	
+		59, 1, stderr);
+
+	return (1);
+}
