30c09c78cf3876a238f7a618963ffe17b2d97054
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-06-03 07:26:25 -0700

    Task #12, correction #1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,31 +7,26 @@
  */
 int main(void)
 {
-	int num1, num2, num3, num4;
+	int num1, num2;
 
-	for (num1 = 0; num1 < 10; num1++)
+	for (num1 = 0; num1 < 100; num1++)
 	{
-		for (num2 = 0; num2 < 10; num2++)
+		for (num2 = 0; num2 < 100; num2++)
 		{
-			for (num3 = num1; num3 < 10; num3++)
-			{
-				for (num4 = num2 + 1; num4 < 10; num4++)
-				{
-					putchar(num1 + '0');
-					putchar(num2 + '0');
-					putchar(' ');
-					putchar(num3 + '0');
-					putchar(num4 + '0');
-					if (num1 == 9 && num2 == 8)
-					{
-						break;
-					}
-					putchar(',');
-					putchar(' ');
-				}
-			}
+			if (num1 == num2 || num2 < num1)
+				continue;
+			putchar(num1 / 10 + '0');
+			putchar(num1 % 10 + '0');
+			putchar(' ');
+			putchar(num2 / 10 + '0');
+			putchar(num2 % 10 + '0');
+			if (num1 == 98 && num2 == 99)
+				break;
+			putchar(',');
+			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
