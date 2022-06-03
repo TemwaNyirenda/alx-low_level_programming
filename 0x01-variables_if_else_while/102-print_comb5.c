978e25194741604e030ae836c71ff8f497e2dbac
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-06-03 07:02:05 -0700

    Task #12, added file

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - prints all dif possible combi of two two-digit numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int num1, num2, num3, num4;
+
+	for (num1 = 0; num1 < 10; num1++)
+	{
+		for (num2 = 0; num2 < 10; num2++)
+		{
+			for (num3 = num1; num3 < 10; num3++)
+			{
+				for (num4 = num2 + 1; num4 < 10; num4++)
+				{
+					putchar(num1 + '0');
+					putchar(num2 + '0');
+					putchar(' ');
+					putchar(num3 + '0');
+					putchar(num4 + '0');
+					if (num1 == 9 && num2 == 8)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
