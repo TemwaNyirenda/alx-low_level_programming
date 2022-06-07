91f26650561329699bcef93d998f194af2ea6a07
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-06-07 10:19:04 -0700

    Task #14, added task file (no header file needed)

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers starting with 1 and 2
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long fib_1 = 1, fib_2 = 2, fib_3, count;
+
+	for (count = 0; count <= 50; count++)
+	{
+		fib_3 = fib_1 + fib_2;
+
+		printf("%lu, ", fib_1);
+
+		fib_1 = fib_2;
+		fib_2 = fib_3;
+	}
+	printf("\n");
+
+	return (0);
+}
