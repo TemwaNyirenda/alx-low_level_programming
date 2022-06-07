40485132d056c5fcd886a435be5d724df301bef8
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-06-07 02:36:28 -0700

    Task #0, added task file and main.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - prints a string using _putchar
+ *
+ * Description: This function prints the string "_putchar\n" 
+ * using function _putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
