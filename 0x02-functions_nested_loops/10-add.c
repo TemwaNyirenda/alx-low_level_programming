0f90b860e7801b3036399f50560716b427c9ef8a
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-06-07 06:57:47 -0700

    Task #10, added task file & header file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds to ints and returns the result
+ * @a: first operand
+ * @b: second operand
+ *
+ * Return: the sum of the two ints
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
