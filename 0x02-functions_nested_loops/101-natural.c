9486c3e80f1403da6d397224439468776f9250b0
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-06-07 08:45:50 -0700

    Task #13, added task file (header not needed)

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - computes and print the sum of all multiples of 3 or 5 less than 1024
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int sum = 0, number;
+
+	for (number = 0; number < 1024; number++)
+	{
+		if (number % 3 == 0)
+		{
+			sum += number;
+		}
+		else if (number % 5 == 0)
+		{
+			sum += number;
+		}
+	}
+
+	printf("%d", sum);
+
+	return (0);
+}
