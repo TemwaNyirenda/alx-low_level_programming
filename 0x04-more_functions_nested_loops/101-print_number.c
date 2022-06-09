7a6ff9fd7d714ecc262a50e9b561ef1338cda7c8
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-06-09 02:55:32 -0700

    Task #12, added task file & header file

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,92 @@
+#include "main.h"
+
+void print_number_continued(int abs_n, int remainder);
+
+/**
+ * print_number - prints an integer using _putchar
+ * @n: the integer to be printed
+ *
+ * Return: void - nothing
+ */
+void print_number(int n)
+{
+	int remainder = n, abs_n;
+
+	if (n < 0)
+		_putchar('-');
+	if (n >= 1000000000 || n <= -1000000000)
+	{
+		if (remainder < 0)
+		{
+			_putchar((remainder / 1000000000) * -1 + '0');
+			remainder = remainder % 1000000000;
+		}
+		else
+		{
+			_putchar(remainder / 1000000000 + '0');
+			remainder = remainder % 1000000000;
+		}
+	}
+	if (n < 0)
+	{
+		remainder = remainder * -1;
+		abs_n = n * -1;
+	}
+	else
+		abs_n = n;
+	if (abs_n >= 100000000)
+	{
+		_putchar(remainder / 100000000 + '0');
+		remainder = remainder % 100000000;
+	}
+	if (abs_n >= 10000000)
+	{
+		_putchar(remainder / 10000000 + '0');
+		remainder = remainder % 10000000;
+	}
+	print_number_continued(abs_n, remainder);
+}
+
+/**
+ * print_number_continued - continues function bc 40 line restriction
+ * @abs_n: the absolute value of n
+ * @remainder: remaining digits to be printed
+ *
+ * Return: void - nothing
+ */
+void print_number_continued(int abs_n, int remainder)
+{
+
+	if (abs_n >= 1000000)
+	{
+		_putchar(remainder / 1000000 + '0');
+		remainder = remainder % 1000000;
+	}
+
+	if (abs_n >= 100000)
+	{
+		_putchar(remainder / 100000 + '0');
+		remainder = remainder % 100000;
+	}
+	if (abs_n >= 10000)
+	{
+		_putchar(remainder / 10000 + '0');
+		remainder = remainder % 10000;
+	}
+	if (abs_n >= 1000)
+	{
+		_putchar(remainder / 1000 + '0');
+		remainder = remainder % 1000;
+	}
+	if (abs_n >= 100)
+	{
+		_putchar(remainder / 100 + '0');
+		remainder = remainder % 100;
+	}
+	if (abs_n >= 10)
+	{
+		_putchar(remainder / 10 + '0');
+		remainder = remainder % 10;
+	}
+	_putchar(remainder % 10 + '0');
+}
