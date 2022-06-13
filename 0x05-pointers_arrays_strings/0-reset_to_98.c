0fe9ced53ed739eba8debb8c76fc265e90852f0d
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-06-13 01:14:00 -0700

    Task #0, added task file & header file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value that pointer n points to to 98
+ * @n: variable that will be updated to 98
+ *
+ * Return: void - nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
