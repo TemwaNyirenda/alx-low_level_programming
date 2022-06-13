d62bf1a551498dd51436f9c0eaeaba94b61fe7eb
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-06-13 01:37:15 -0700

    Task #3, added task file and header file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints string to stdout
+ * @str: string to be printed
+ *
+ * Return: void - nothing
+ */
+void _puts(char *str)
+{
+	int index = 0;
+
+	while (*(str + index) != '\0')
+	{
+		_putchar(*(str + index));
+		index++;
+	}
+	_putchar('\n');
+}
