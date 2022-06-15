99d6e13c5dbd3e8261ef91f5f5d40d78c32e94c0
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-06-15 14:20:54 -0700

    Task #3, correction #1

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,9 +11,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int index;
 
-	for (index = 0; s1[index - 1] != '\0' || s2[index - 1] != '\0';
-			index++)
+	for (index = 0; ; index++)
 	{
+		if (s1[index] == '\0' && s2[index] == '\0')
+			break;
 		if (s1[index] != s2[index])
 			return (s1[index] - s2[index]);
 	}
