7600182b3a8823dcaecf9b9bc8739d6155c7a0d4
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-06-22 12:57:47 -0700

    Task #5, added task file & header file

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+int find_sqrt(int n, int sqrt);
+
+/**
+ * _sqrt_recursion - calculates the natural square root of a number n
+ * @n: the number of which the natural square root will be calculated
+ *
+ * Return: -1 if no natural square root is found, the square root otherwise
+ */
+int _sqrt_recursion(int n)
+{
+	return (find_sqrt(n, n));
+}
+
+/**
+ * find_sqrt - finds the square root of n recursively
+ * @n: the number that the natural square root will be found
+ * @sqrt: the trial numbers to see if it's the square root
+ *
+ * Return: -1 if no natural square root is found, the square root otherwise
+ */
+int find_sqrt(int n, int sqrt)
+{
+	if (sqrt < 0)
+		return (-1);
+	if (sqrt * sqrt == n)
+		return (sqrt);
+
+	return (find_sqrt(n, sqrt - 1));
+}
