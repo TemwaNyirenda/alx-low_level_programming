08727d9f38706916a90c643561eea41e5e0e5e9e
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-06-30 03:57:25 -0700

    Task #2, correction #1 - initialized calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+char *_memset(char *s, char b, unsigned int n);
+
 /**
  * _calloc - allocates memory to an array of nmemb elements of size size each
  * @nmemb: the number of elements in the array
@@ -20,5 +22,27 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (ptr == NULL)
 		return (NULL);
 
+	ptr = _memset(ptr, 0, nmemb);
+
 	return (ptr);
+}
+
+/**
+ * _memset - fills memory with a constant byte, b
+ * @s: the memory area to be filled
+ * @b: the constant byte that will fill the memory
+ * @n: the amount of bytes in memory area s will be filled with b
+ *
+ * Return: a point to memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int index;
+
+	for (index = 0; index < n; index++)
+	{
+		s[index] = b;
+	}
+
+	return (s);
 }
