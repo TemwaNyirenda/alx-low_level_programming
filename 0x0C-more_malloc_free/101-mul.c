912c27efaa64421fd377f137938b5956e628d4f0
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-07-01 17:28:24 -0700

    Task #5, fixed betty issues

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stddef.h>
-#include <stdio.h>
 
 void error_message(void);
 int is_only_digits(char *str);
@@ -9,18 +8,19 @@ int _strlen(char *str);
 int convert_char_to_int(char c);
 int is_num_zero(int *array, int len);
 void multiply_int_array(int *array1, int *array2, int len1, int len2);
-int *_memset(int *s, int b, unsigned int n);
+void add_array_columns(int **array, int row, int col);
 
 /**
  * main - multiplies two numbers passed as arguments on the command line
- * argc: the number of arguments
- * argv: the pointer to an array of arguments
+ * @argc: the number of arguments
+ * @argv: the pointer to an array of arguments
  *
  * Return: the two numbers multiplied
  */
 int main(int argc, char *argv[])
 {
 	int j, k, *ptr_num1, *ptr_num2;
+
 	if (argc != 3)
 		error_message();
 
@@ -65,7 +65,7 @@ void error_message(void)
 {
 	int i;
 	char *error_message = "Error\n";
-			
+
 	for (i = 0; i < 6; i++)
 		_putchar(error_message[i]);
 	exit(98);
@@ -94,7 +94,7 @@ int is_only_digits(char *str)
  * _strlen - calculates the string length
  * @str: the string used in the calculation
  *
- * Return; the string length
+ * Return: the string length
  */
 int _strlen(char *str)
 {
@@ -150,7 +150,7 @@ int is_num_zero(int *array, int len)
 {
 	int i;
 
-	for(i = 0; i < len; i++)
+	for (i = 0; i < len; i++)
 	{
 		if (array[i] != 0)
 			return (1);
@@ -165,134 +165,89 @@ int is_num_zero(int *array, int len)
  * @array2: second array in multiplication
  * @len1: length of first array
  * @len2: length of second array
- * @tlen: length of new array to hold multiplication
  *
  * Return: nothing - void
  */
 void multiply_int_array(int *array1, int *array2, int len1, int len2)
 {
-
-	int i, j, k, l, mul, sum = 0, carry_over = 0, mul_c_o, col;
-	int **ptr2mul, *ptr2mul_sum; 
+	int i, j, k, l, mul, carry_over = 0, mul_c_o, col, **ptr2mul;
 
 	if (len1 > len2) /* col is columns in 2D array */
 		col = len1 * 2;
 	else
 		col = len2 * 2;
-		
-	ptr2mul_sum = malloc(sizeof(int) * col);
-	if (ptr2mul_sum == NULL)
-		error_message();
 
 	ptr2mul = malloc(sizeof(int *) * len2); /* len2 number of rows */
 	if (ptr2mul == NULL)
-	{
-		free(ptr2mul_sum);
 		error_message();
-	}
-
 	for (i = 0; i < len2; i++)
 	{
 		ptr2mul[i] = malloc(sizeof(int) * col);
 		if (ptr2mul[i] == NULL)
 		{
-			free(ptr2mul_sum);
-			free(ptr2mul);
-			while (i >= 0)
-			{
+			for (; i >= 0; i--)
 				free(ptr2mul[i]);
-				i--;
-			}
+			free(ptr2mul);
 			error_message();
 		}
 	}
-	
-	
-/*
-	printf("%d\n", len1);
-	printf("%d\n", len2);
-	printf("%d\n", tlen);
-	for (i = 0; i < len1; i++)
-		printf("%d\n", array1[i]);
-	for (i = 0; i < len2; i++)
-		printf("%d\n", array2[i]);
-*/
-/*	if (len1 < len2)
-		max_len = len1;
-	else
-		max_len = len2;
-*/
 	for (j = len2 - 1, k = 0; k < len2; j--, k++)
 	{
 		for (i = len1 - 1, l = k; l < col; i--, l++)
 		{
-
 			if (i < 0)
 				mul = 0;
 			else
 				mul = array1[i] * array2[j];
-/*			printf("\t\tarray1[%d] = %d, array2[%d] = %d, mul = %d\n", i, array1[i], j, array2[j], mul);
-*/			mul_c_o = mul + carry_over;
-
+			mul_c_o = mul + carry_over;
 			ptr2mul[k][l] = mul_c_o % 10;
 			carry_over = mul_c_o / 10;
-/*			printf("\t ptr2mul[%d][%d] = %d and carryover = %d\n", k, l, ptr2mul[k][l], carry_over);
-*/		}
+		}
 	}
+	add_array_columns(ptr2mul, len2, col);
+	for (i = 0; i < len2; i++)
+		free(ptr2mul[i]);
+	free(ptr2mul);
+}
 
-	carry_over = 0;
+/**
+ * add_array_columns - adds the columns of a 2D array
+ * @array: the array that will be used in the addition
+ * @col: number of columns in the array
+ * @row: number of rows in the array
+ *
+ * Return: nothing
+ */
+void add_array_columns(int **array, int row, int col)
+{
+	int l, k, carry_over = 0, sum = 0, *ptr2mul_sum;
+
+	ptr2mul_sum = malloc(sizeof(int) * col);
+	if (ptr2mul_sum == NULL)
+		error_message();
 
 	for (l = 0; l < col; l++)
 	{
 		sum = carry_over;
-		for(k = 0; k < len2; k++)
+		for (k = 0; k < row; k++)
 		{
-			sum += ptr2mul[k][l];
+			sum += array[k][l];
 			carry_over = sum / 10;
 			ptr2mul_sum[l] = sum % 10;
-/*			printf("l = %d, k = %d, array = %d, sum = %d, carryover = %d\n", l, k, ptr2mul[k][l], sum, carry_over);
-*/		}
+		}
 	}
 
 	for (l = col - 1; l >= 0; l--)
 	{
 		if (l == col - 1 && ptr2mul_sum[l] == 0)
 		{
-/*			printf("\t\t\tptr2mul_sum[%d] = %d\n", l, ptr2mul_sum[l]);
-*/			while (ptr2mul_sum[l] == 0)
-			       l--;
-	
+			while (ptr2mul_sum[l] == 0)
+				l--;
 		}
 
 		_putchar(ptr2mul_sum[l] + 48);
 	}
 	_putchar('\n');
-	
+
 	free(ptr2mul_sum);
-	free(ptr2mul);
-	for (i = 0; i < len2; i++)
-	{
-		free(ptr2mul[i]);
-	}
 }
-
-/**
- * _memset - fills memory with a constant byte, b
- * @s: the memory area to be filled
- * @b: the constant byte that will fill the memory
- * @n: the amount of bytes in memory area s will be filled with b
- *
- * Return: a point to memory area s
- */
-int *_memset(int *s, int b, unsigned int n)
-{
-	unsigned int index;
-
-	for (index = 0; index < n; index++)
-	{
-		s[index] = b;
-	}
-
-	return (s);
-}
-
