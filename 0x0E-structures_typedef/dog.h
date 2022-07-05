18b2d046ec17eee3392974d7cadcdfdaf05dda5f
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-07-04 19:39:26 -0700

    Task #3, added task header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,12 @@
  * @age: the age of the dog
  * @owner: the dog's owner's name
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
