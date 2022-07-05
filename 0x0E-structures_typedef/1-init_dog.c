c118ee529f994112307883c47daf66e3bc2ac647
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-07-04 19:02:16 -0700

    Task #1, correction #1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
  * init_dog - initializes variable d of type struct dog
@@ -11,6 +13,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		exit(1);
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
