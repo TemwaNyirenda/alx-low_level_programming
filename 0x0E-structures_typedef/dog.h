9eea71c0cfc3af6f6d17a88fe02141ef37527f45
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-07-04 18:45:23 -0700

    Task #0, added a betty description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a structure listing the name, age, and owner of a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the dog's owner's name
+ */
 struct dog
 {
 	char *name;
