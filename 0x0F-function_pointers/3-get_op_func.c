4c02122f4b9d385667859f07df17784dd7c8aa38
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-07-06 06:40:31 -0700

    Task #3, added three .c files and one header file

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * get_op_func - pointer to a function. Function is determined by string s
+ * @s: the string used to determine which function to use in return
+ *
+ * Return: int calculated through function that was chosen to be pointed to,
+ *	NULL if string s is not "+", "-", "*", "/", or "%"
+ */
+int (*get_op_func(char *s))(int a, int b)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			break;
+		i++;
+	}
+	if (i == 5)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	return (ops[i].f);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - performs simple operations,
+ *	like addition, subtraction, multiplication, division, and modulous,
+ *	depending on what arguments are passed to the function
+ * @argc: the number of arguments passed to main
+ * @argv: the string array of the arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0) && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", (*get_op_func(argv[2]))(a, b));
+
+	return (0);
+}
