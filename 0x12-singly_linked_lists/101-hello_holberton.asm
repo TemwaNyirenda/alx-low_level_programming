6feb8ebb5d74631fc0b9a7dcd17d8b586bc8cb77
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-07-14 18:36:30 -0700

    Task #6, redid assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,23 +1,17 @@
+section .data
+	msg:	db "Hello, Holberton", 0
+	fmt:	db "%s", 10, 0
+
+
+section .text
 global main
 extern printf
 
-section .text
 main:
-    push rbp           ; Keep the stack aligned
-    mov  rbp, rsp
-    sub rsp, 16        ;//must be 16 byte increments  to maintain state alignment
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-
-    ;write our body
-    xor eax, eax        ;
-    lea rdi, [msg]      ;first parameter
-    call printf
-
-
-    add rsp, 16
-    leave               ; mov rsp, rbp, pop rbp
-    ret
-
-section .data
-   msg: db "Hello, Holberton",10
-
+	mov ebx, 0
+	mov eax, 1
