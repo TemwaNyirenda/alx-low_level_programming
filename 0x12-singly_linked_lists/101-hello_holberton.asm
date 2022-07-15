75ff6a26b15f78b40b167315c97388bec67a499f
TemwaNyirenda <temwanyirenda00@gmail.com>
2022-07-14 18:12:54 -0700

    Task #6, created 101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,22 @@
+global main
+extern printf, scanf
+
+section .text
+main:
+    push rbp           ; Keep the stack aligned
+    mov  rbp, rsp 
+    sub rsp, 16        ;//must be 16 byte increments  to maintain state alignment
+
+
+    ;write our body
+    xor eax, eax        ;
+    lea rdi, [msg]      ;first parameter
+    call printf 
+
+
+    add rsp, 16 
+    leave               ; mov rsp, rbp, pop rbp
+    ret
+
+section .data
+   msg: db "Hello, Holberton",10
